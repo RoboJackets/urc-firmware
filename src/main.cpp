e092a9a184d7c3f8cbdd9431d7dec4a8aaead1fe
AR2100 <andrewroachpublic@gmail.com>
2022-07-20 22:17:36 -0400

    roboclaw library working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <Arduino.h>
+
 #include "pb_encode.h"
 #include "pb_decode.h"
-
 #include "urc.pb.h"
 
+#include <SoftwareSerial.h>
+#include "RoboClaw.h"
+
+#define address 0x80
+
+//Velocity PID coefficients.
+#define Kp 1.0
+#define Ki 0.5
+#define Kd 0.25
+#define qpps 44000
+
+RoboClaw roboclaw(&Serial2,10000);
+
+void roboclawTest();
+void nanopbTest();
+
 void setup() {
+  
+  nanopbTest(); 
+  roboclawTest();
+}
+
+void nanopbTest() {
   Serial.begin(9600);
   Serial.println("Hello World!");
 
@@ -30,6 +52,13 @@ void setup() {
   response_length = ostream.bytes_written;
 
   Serial.printf("Message length: %d\n", response_length);
+}
+
+void roboclawTest() {
+  roboclaw.begin(38400);
+  roboclaw.SetM1VelocityPID(address,Kd,Kp,Ki,qpps);
+  roboclaw.SetM2VelocityPID(address,Kd,Kp,Ki,qpps);  
+  return;
 }
 
 void loop() {
