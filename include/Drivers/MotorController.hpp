23808fcf635a4c0893e8b5db4d72835319395c68
Pranav Tadepalli <orca.pranav@gmail.com>
2023-01-15 21:45:01 -0500

    Revert "mushroom"
    This reverts commit e974a9fa249bf7b3cdd4a2d5eacfdbb9a47ee2a6.

--- include/Drivers/MotorController.hpp
+++ include/Drivers/MotorController.hpp
@@ -8,30 +8,13 @@ namespace motors {
 
 class MotorController {
 public:
-  bool zeroPosition = 0;
-
   MotorController(){};
-
   virtual void begin(long speed);
   // TODO: virtual void setMode(uint8_t channel, MotorMode mode) = 0;
   virtual int32_t getSpeed(uint8_t address, uint8_t channel, bool &valid) = 0;
   virtual void setSpeed(int32_t speed, uint8_t address, uint8_t channel, bool &valid) = 0;
-
-  virtual int32_t getRawPosition(uint8_t address, uint8_t channel, bool &valid) = 0;
-  virtual void setRawPosition(int32_t position, uint8_t address, uint8_t channel, bool &valid) = 0;
-
-  virtual int32_t getPosition(uint8_t address, uint8_t channel, bool &valid) {
-    return getRawPosition(address, channel, valid) - zeroPosition;
-  }
-
-  virtual void setPosition(int32_t position, uint8_t address, uint8_t channel, bool &valid) {
-    setRawPosition(position + zeroPosition, address, channel, valid);
-  }
-
-  virtual void zero(uint8_t address, uint8_t channel, bool &valid){
-    zeroPosition = getRawPosition(address, channel, valid);
-  }
-
+  // TODO: virtual int32_t getPosition(uint8_t channel) = 0;
+  // TODO: virtual void setPosition(uint8_t channel) = 0;
 };
 
 }  // namespace motors
