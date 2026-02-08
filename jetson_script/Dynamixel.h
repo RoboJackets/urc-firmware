efb746ca53e1041bdcf314c4d837797be98db863
Henry Hajdari <henry05.haj@gmail.com>
2026-02-08 17:39:13 -0500

    changed motorcontro.h to dynamixel.h

--- jetson_script/Dynamixel.h
+++ jetson_script/Dynamixel.h
@@ -19,15 +19,181 @@
 #include "pb_decode.h" // Protobuf decode functions
 
 // UPDATE AS WE GO ALONG
-class MotorControl : public Motors {
-    public:
-        void setSpeed() override;
-        void getSpeed() override;
-        void setPosition() override;
-        void getPosition() override;
-        void setEffort() override;
-        void getEffort() override;
-        void stop() override;
-};
+// class MotorControl : public Motors {
+//     public:
+//         void setSpeed() override;
+//         void getSpeed() override;
+//         void setPosition() override;
+//         void getPosition() override;
+//         void setEffort() override;
+//         void getEffort() override;
+//         void stop() override;
+// };
+
+int main() {
+    /**********************START OF DYNAMIXEL MAIN FUNCTION**************************/
+  // Initialize PortHandler instance
+  // Set the port path
+  // Get methods and members of PortHandlerLinux or PortHandlerWindows
+  dynamixel::PortHandler *portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+
+  // Initialize PacketHandler instance
+  // Set the protocol version
+  // Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
+  dynamixel::PacketHandler *packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
+
+  int index = 0;
+  int dxl_comm_result = COMM_TX_FAIL;             // Communication result
+  int dxl_goal_position[2] = {MINIMUM_POSITION_LIMIT, MAXIMUM_POSITION_LIMIT}; // Goal position
+
+  uint8_t dxl_error = 0; // DYNAMIXEL error
+  #if defined(XL320)
+  int16_t dxl_present_position = 0;  // XL-320 uses 2 byte Position data
+  #else
+  int32_t dxl_present_position = 0;  // Read 4 byte Position data
+  #endif
+
+  // Open port
+  if (portHandler->openPort()) {
+    printf("Succeeded to open the port!\n");
+  }
+  else {
+    printf("Failed to open the port!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Set port baudrate
+  if (portHandler->setBaudRate(BAUDRATE)) {
+    printf("Succeeded to change the baudrate!\n");
+  }
+  else {
+    printf("Failed to change the baudrate!\n");
+    printf("Press any key to terminate...\n");
+    getch();
+    return 0;
+  }
+
+  // Enable DYNAMIXEL Torque
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_ENABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS) {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0) {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  else {
+    printf("Succeeded enabling DYNAMIXEL Torque.\n");
+  }
+
+  while(1) {
+    printf("Press any key to continue. (Press [ESC] to exit)\n");
+    if (getch() == ESC_ASCII_VALUE)
+      break;
+
+    // Write goal position
+    #if defined(XL320)  // XL-320 uses 2 byte Position data
+    dxl_comm_result = packetHandler->write2ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, dxl_goal_position[index], &dxl_error);
+    #else
+    dxl_comm_result = packetHandler->write4ByteTxRx(portHandler, DXL_ID, ADDR_GOAL_POSITION, dxl_goal_position[index], &dxl_error);
+    #endif
+    if (dxl_comm_result != COMM_SUCCESS) {
+      printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+    else if (dxl_error != 0) {
+      printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+    }
+
+    do {
+      // Read the Present Position
+      #if defined(XL320)  // XL-320 uses 2 byte Position data
+      dxl_comm_result = packetHandler->read2ByteTxRx(portHandler, DXL_ID, ADDR_PRESENT_POSITION, (uint16_t*)&dxl_present_position, &dxl_error);
+      #else
+      dxl_comm_result = packetHandler->read4ByteTxRx(portHandler, DXL_ID, ADDR_PRESENT_POSITION, (uint32_t*)&dxl_present_position, &dxl_error);
+      #endif
+      if (dxl_comm_result != COMM_SUCCESS) {
+        printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+      }
+      else if (dxl_error != 0) {
+        printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+      }
+
+      printf("[ID:%03d] Goal Position:%03d  Present Position:%03d\n", DXL_ID, dxl_goal_position[index], dxl_present_position);
+
+    } while((abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD));
+
+    // Switch the Goal Position
+    if (index == 0) {
+      index = 1;
+    }
+    else {
+      index = 0;
+    }
+  }
+
+  // Disable DYNAMIXEL Torque
+  dxl_comm_result = packetHandler->write1ByteTxRx(portHandler, DXL_ID, ADDR_TORQUE_ENABLE, TORQUE_DISABLE, &dxl_error);
+  if (dxl_comm_result != COMM_SUCCESS) {
+    printf("%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  else if (dxl_error != 0) {
+    printf("%s\n", packetHandler->getRxPacketError(dxl_error));
+  }
+  else {
+    printf("Succeeded disabling DYNAMIXEL Torque.\n");
+  }
+
+  // Close port
+  portHandler->closePort();
+  return 0;
+}
+
+// Dynamixel-made method
+int getch() {
+#if defined(__linux__) || defined(__APPLE__)
+  struct termios oldt, newt;
+  int ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+#elif defined(_WIN32) || defined(_WIN64)
+  return _getch();
+#endif
+}
+
+// Dynxamiel-made method
+int kbhit(void) {
+#if defined(__linux__) || defined(__APPLE__)
+  struct termios oldt, newt;
+  int ch;
+  int oldf;
+
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+  fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+  ch = getchar();
+
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+  if (ch != EOF) {
+    ungetc(ch, stdin);
+    return 1;
+  }
+
+  return 0;
+#elif defined(_WIN32) || defined(_WIN64)
+  return _kbhit();
+#endif
+}
 
 #endif
