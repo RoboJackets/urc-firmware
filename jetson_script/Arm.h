0d5d6f8aa38aafccd08ccf17d3c38fb4274a50f0
Henry Hajdari <henry05.haj@gmail.com>
2026-02-11 21:00:42 -0500

    Started adding methods from easy.sdk

--- jetson_script/Arm.h
+++ jetson_script/Arm.h
@@ -1,34 +1,17 @@
-/*********************DYNAMIXEL LIBRARIES***********************/
-/*********************TAKEN FROM DYNAMIXEL SDK*********************/
-#if defined(__linux__)
-#include <fcntl.h>
-#include <termios.h>
-#define STDIN_FILENO 0
-#elif defined(_WIN64)
-#include <conio.h>
-#endif
+/**************************GENERAL LIBRARIES**************************/
+/*******************TO BE UTILIZED FOR ALL MOTORS*******************/
 
-#include <stdlib.h>
-#include <stdio.h>
+#include <Arduino.h>
+#include <FlexCAN_T4.h> // CAN enabling tool
+#include "SoloCAN.hpp" // SOLO UNO motor controller's library
+#include <QNEthernet.h> // Teensy's ethernet library
+#include <map>
 
-#include "dynamixel_sdk.h"  // Uses DYNAMIXEL SDK library
-#define PROTOCOL_VERSION  2.0 // Dynamixel Protocol Version
-#define DXL_ID  1 // Default ID for Dynamixel servos
-#define DEVICENAME  "/dev/ttyUSB0" // Linux port assigned to U2D2 (USB-to-serial converter)
+#include "urc.ph.h" // Protobuf header file for compiling code
+#include "Messages.hpp" // Protobuff decode functions (encodeResponse, decodeResponse)
 
-#define X_SERIES // Type of Dynamixel servos we are using
-
-#define ADDR_TORQUE_ENABLE          64
-#define ADDR_GOAL_POSITION          116
-#define ADDR_PRESENT_POSITION       132
-#define MINIMUM_POSITION_LIMIT      0  // Refer to the Minimum Position Limit of product eManual
-#define MAXIMUM_POSITION_LIMIT      4095  // Refer to the Maximum Position Limit of product eManual
-#define BAUDRATE                    57600
-
-#define TORQUE_ENABLE                   1
-#define TORQUE_DISABLE                  0
-#define DXL_MOVING_STATUS_THRESHOLD     20  // DYNAMIXEL moving status threshold
-#define ESC_ASCII_VALUE                 0x1b
+#include "pb_encode.h" // Protobuf encode functions
+#include "pb_decode.h" // Protobuf decode functions
 
 int getch(); // Dynamixel SDK function
 int kbhit(void); // Dynamixel SDK function
--- jetson_script/Dynamixel.h
+++ jetson_script/Dynamixel.h
@@ -2,24 +2,43 @@
 #define MOTORCONTROL_H
 
 #include "Motors.h"
+#include "dynamixel_easy_sdk/motor.hpp"
+#include "dynamixel_easy_sdk/connector.hpp"
 
-/**************************GENERAL LIBRARIES**************************/
-/*******************TO BE UTILIZED FOR ALL MOTORS*******************/
+/************************DYNAMIXEL LIBRARIES************************/
+/*****************TO BE USED WITH DYNAMIXEL SERVOS***********************/
+#if defined(__linux__)
+#include <fcntl.h>
+#include <termios.h>
+#define STDIN_FILENO 0
+#elif defined(_WIN64)
+#include <conio.h>
+#endif
 
-#include <Arduino.h>
-#include <FlexCAN_T4.h> // CAN enabling tool
-#include "SoloCAN.hpp" // SOLO UNO motor controller's library
-#include <QNEthernet.h> // Teensy's ethernet library
-#include <map>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "urc.ph.h" // Protobuf header file for compiling code
-#include "Messages.hpp" // Protobuff decode functions (encodeResponse, decodeResponse)
+#include "dynamixel_sdk.h"  // Uses DYNAMIXEL SDK library
+#define PROTOCOL_VERSION  2.0 // Dynamixel Protocol Version
+#define DXL_ID  1 // Default ID for Dynamixel servos
+#define DEVICENAME  "/dev/ttyUSB0" // Linux port assigned to U2D2 (USB-to-serial converter)
 
-#include "pb_encode.h" // Protobuf encode functions
-#include "pb_decode.h" // Protobuf decode functions
+#define X_SERIES // Type of Dynamixel servos we are using
+
+#define ADDR_TORQUE_ENABLE          64
+#define ADDR_GOAL_POSITION          116
+#define ADDR_PRESENT_POSITION       132
+#define MINIMUM_POSITION_LIMIT      0  // Refer to the Minimum Position Limit of product eManual
+#define MAXIMUM_POSITION_LIMIT      4095  // Refer to the Maximum Position Limit of product eManual
+#define BAUDRATE                    57600
+
+#define TORQUE_ENABLE                   1
+#define TORQUE_DISABLE                  0
+#define DXL_MOVING_STATUS_THRESHOLD     20  // DYNAMIXEL moving status threshold
+#define ESC_ASCII_VALUE                 0x1b
 
 // UPDATE AS WE GO ALONG
-// class MotorControl : public Motors {
+// class Dynamixel : public Motors {
 //     public:
 //         void setSpeed() override;
 //         void getSpeed() override;
@@ -29,6 +48,8 @@
 //         void getEffort() override;
 //         void stop() override;
 // };
+
+enableTorque();
 
 int main() {
     /**********************START OF DYNAMIXEL MAIN FUNCTION**************************/
